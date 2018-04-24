d03eccbfa3e28c8c608c1e974fbf598fbf6bce76
wangxiaojing2413 <18752031024@163.com>
2018-04-24 21:09:31 +0800

    nio denos

---
+++ NIO/src/nio/NIODemo.java
@@ -0,0 +1,61 @@
+package nio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+public class NIODemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void method1(){
+        RandomAccessFile aFile = null;
+        try{
+            aFile = new RandomAccessFile("src/nio.txt","rw");
+            FileChannel fileChannel = aFile.getChannel();
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+ 
+            int bytesRead = fileChannel.read(buf);
+            System.out.println(bytesRead);
+ 
+            while(bytesRead != -1)
+            {
+                buf.flip();
+                while(buf.hasRemaining())
+                {
+                    System.out.print((char)buf.get());
+                }
+ 
+                buf.compact();
+                bytesRead = fileChannel.read(buf);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(aFile != null){
+                    aFile.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+	
+	
+}
